f96d013987e6f5224cfacfa0739d1ddbbd27a255
mumuniazeez <mumuniazeez99@gmail.com>
2024-12-20 11:10:27 +0100

    added file upload endpoint

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import userRouter from "./router/users/router.js";
 import adminRouter from "./router/admins/router.js";
 import migrate from "./migrate.js";
 import rateLimit from "express-rate-limit";
+import { handleUpload } from "@vercel/blob/client";
+import { authenticateAdmin } from "./util/util.js";
 
 config();
 /**
@@ -43,6 +45,42 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(userRouter);
 app.use("/admin", adminRouter);
+
+app.post("/admin/handleUpload", async (req, res) => {
+  const { body } = req;
+
+  try {
+    const jsonResponse = await handleUpload({
+      body,
+      request: req,
+      onBeforeGenerateToken: async (pathname /*, clientPayload */) => {
+        return {
+          allowedContentTypes: ["image/*", "video/*"],
+          tokenPayload: JSON.stringify({}),
+        };
+      },
+      onUploadCompleted: async ({ blob, tokenPayload }) => {
+        // Get notified of client upload completion
+        // ⚠️ This will not work on `localhost` websites,
+        // Use ngrok or similar to get the full upload flow
+
+        console.log("blob upload completed", blob, tokenPayload);
+
+        try {
+          // Run any logic after the file upload completed,
+          // If you've already validated the user and authorization prior, you can
+          // safely update your database
+        } catch (error) {
+          throw new Error("Could not update post");
+        }
+      },
+    });
+
+    return res.json(jsonResponse);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
 
 await migrate();
 
